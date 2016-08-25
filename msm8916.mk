f646125480a753ec208e89e2efc8bc87c8e7705a
Adrian DC <radian.dc@gmail.com>
2016-08-25 03:43:29 +0200

    msm8916-common: Add AOSP products and properties
     * Builds non default AOSP products:
        Launcher3, messaging, SoundRecorder,
        SpeechRecorder, Terminal
    
     * Prevent Google SetupWizard from rotating
     * Show storage manager settings
    
     * Add default overlay overrides for Settings
        gestures and storage manager visibility and
        enable productivity_device for DocumentsUI
    
    Change-Id: I830c861404ff5ce943507952ea1c31088b9aca97
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -280,6 +280,19 @@ PRODUCT_PACKAGES += \
     init.power.rc \
     ueventd.rc
 
+# AOSP packages
+PRODUCT_PACKAGES += \
+    Launcher3 \
+    messaging \
+    SoundRecorder \
+    SpeechRecorder \
+    Terminal
+
+# AOSP properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.setupwizard.rotation_locked=true \
+    ro.storage_manager.enabled=true
+
 # OEM Unlock
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.oem_unlock_supported=0
