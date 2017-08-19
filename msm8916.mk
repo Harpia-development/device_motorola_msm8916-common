ed673a202b5b4c03490580dbe5df6ad7d0acfafa
Luca Stefani <luca020400@lineageos.org>
2017-08-19 12:10:27 +0200

    gps: Add get_process_name shim
    Change-Id: I10ce654b14866d41610c7e4098df9691b983d515

--- msm8916.mk
+++ msm8916.mk
@@ -123,7 +123,8 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
-    gps.msm8916
+    gps.msm8916 \
+    libshims_get_process_name
 
 # HIDL
 PRODUCT_COPY_FILES += \
