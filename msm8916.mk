1893fe8acc7c5eedd8a5ba9f61768600fe1ffbaf
DroidFreak32 <rushabshah32@gmail.com>
2017-09-17 13:56:28 +0530

    Build Memtrack HAL

--- msm8916.mk
+++ msm8916.mk
@@ -160,6 +160,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# Memtrack HAL
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl
+
 # OMX
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
