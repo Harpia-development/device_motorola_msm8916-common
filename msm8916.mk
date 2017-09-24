6914a2276e62ac22fe17471b2ab6636651bb3f11
Demon000 <demonsingur@gmail.com>
2017-09-24 16:30:56 +0200

    msm8916-common : Convert lights HAL to native binderized HAL
    @SubhrajyotiSen: Adapted from harpia
    
    Change-Id: I4ab5b015c31221fce67264652d1755c724b8e328
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -162,7 +162,7 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
-    lights.msm8916
+    android.hardware.light@2.0-service.harpia
 
 # LineageActions
 PRODUCT_PACKAGES += \
