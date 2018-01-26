de2ce10c012962c12a0fc2a06f2b63d347b69b34
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2018-01-26 05:09:25 +0000

    msm8916-common: BC: use GCC toolchain

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,6 +52,8 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8916
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
