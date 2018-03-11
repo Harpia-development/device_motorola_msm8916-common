79e4414572a4af8bbce84fc35ffb103c4094a881
Kshitij Gupta <kshitijgm@gmail.com>
2018-03-11 14:38:08 +0530

    msm8916-common: Add haxx for faceunlock support
    This is a squashed commit of:
    
    Revert "Revert facelock Haxx"
    
    This reverts commit 1c2924f14388f54aaf69e4a88766eb334b2b57f8.
    Revert facelock Haxx
    
     This haxx has been merged in opengapps
    
    Revert "msm8916-common: Mount 32 bit libfacenet.so on faulty 64 bit lib"
    
    This reverts commit eece668e6e204abbfc6e14078b777586335aa0e4.
    
    Revert "msm8916-common: Include 32 bit libfacenet.so"
    
    This reverts commit ff4e60d22db6b8998550a9447b64efe869caea01.
    
    Revert "msm8916-common: Haxx for faceunlock"
    
    This reverts commit 7946bea8b2ca8995b09fbe33ba8827ffdc24c686.
    
    Change-Id: Ief48208f1593522fc757d519d7fbaac7023cb90c
    
    # Conflicts:
    #	proprietary-files.txt
    msm8916-common: Mount 32 bit libfacenet.so on faulty 64 bit lib
    
    - Since arm gapps now ship with the 64 bit libfacenet.so, we now
       mount the 32 bit libfacenet.so from /vendor to effectively
       shadow the faulty 64 bit copy in /system
    - Resolve denial for the same:
       [   22.909076] type=1400 audit(6996876.503:4): avc: denied { mounton } for pid=1 comm="init" path="/system/lib/libfacenet.so" dev="mmcblk0p14" ino=15427 scontext=u:r:init:s0 tcontext=u:object_r:system_file:s0 tclass=file permissive=0
    
    Change-Id: I4b6bf14b99c638042890a1c1ba60ef826761335e
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    msm8916-common: Haxx for faceunlock
    
    - The last 32 bit libfacenet.so from google appears to be incompatible
      with current protobuf libs
    - Add up the pre-O libprotobuf-cpp lib and shim libfacenet.so with it,
      using LD_SHIM_LIBS
    
    Change-Id: I3b61751d1b8adb9d9c7876f08586d947178ff52f
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    # Conflicts:
    #	rootdir/etc/init.qcom.rc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -138,7 +138,8 @@ TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib/libmmcamera_wavelet_lib.so|libc_util.so \
     /system/vendor/lib/libizat_core.so|libshims_get_process_name.so\
     /system/vendor/lib/libmmqjpeg_codec.so|libboringssl-compat.so \
-    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so
+    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
+    /system/lib/libfacenet.so|libprotobuf-cpp-haxx.so
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
