76d0ffffb84f476af444f616e495cf447a3afff0
Kyle Harrison <khwebmail@gmail.com>
2018-03-27 13:40:31 +0100

    msm8916-common: Binderize Audio HIDL

--- msm8916.mk
+++ msm8916.mk
@@ -54,7 +54,9 @@ PRODUCT_AAPT_CONFIG := normal
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
