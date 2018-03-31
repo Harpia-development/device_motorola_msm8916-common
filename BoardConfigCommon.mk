e9d5f319ce7600ff4081e297e46bc07c47807772
Mati Halperin <mati@cypheros.co>
2018-03-31 14:32:33 -0300

    msm8916-common: Add BoringSSL compatibility library
    Improves camera quality
    
    # Conflicts:
    #	BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -133,7 +133,9 @@ TARGET_LD_SHIM_LIBS := \
     /system/lib/libmot_sensorlistener.so|libsensor.so \
     /system/lib/libjustshoot.so|libshims_camera.so \
     /system/vendor/lib/libmmcamera_wavelet_lib.so|libc_util.so \
-    /system/vendor/lib/libizat_core.so|libshims_get_process_name.so
+    /system/vendor/lib/libizat_core.so|libshims_get_process_name.so\
+    /system/vendor/lib/libmmqjpeg_codec.so|libboringssl-compat.so \
+    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
--- msm8916.mk
+++ msm8916.mk
@@ -89,6 +89,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.msm8916 \
+    libboringssl-compat \
     libc_util \
     libshims_camera \
     Snap
