fddf4d971925440e95e4cd4669b34a270d66d643
Kyle Harrison <khwebmail@gmail.com>
2018-05-12 20:43:41 +0100

    msm8916-common: Move GPS config files from vendor

--- msm8916.mk
+++ msm8916.mk
@@ -145,6 +145,12 @@ PRODUCT_PACKAGES += \
     gps.msm8916 \
     libshims_get_process_name
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf
+
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
