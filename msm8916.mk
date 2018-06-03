ddba9bab0bb27f60ceb09388d19c64928c7f4267
SubhrajyotiSen <subhrajyoti12@gmail.com>
2018-06-03 13:27:14 +0200

    msm8916-common: add soundtrigger HAL

--- msm8916.mk
+++ msm8916.mk
@@ -57,6 +57,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@2.0-service \
+    android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
