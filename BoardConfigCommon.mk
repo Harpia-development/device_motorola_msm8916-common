b7375f2ac4f50c5557bdb03365fb342f8ba853c3
SubhrajyotiSen <subhrajyoti12@gmail.com>
2018-06-09 20:06:05 +0200

    msm8916-common: use exfat driver

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -57,6 +57,7 @@ TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8916
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
+TARGET_EXFAT_DRIVER := exfat
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
