97004cc61c9921909674490138d2e928b62840db
Michael W <baddaemon87@gmail.com>
2018-06-23 17:15:03 +0200

    msm8916-common: Don't attempt to build copybit
    * The barrier doesn't even allow building it on 8916/3939
    * See https://github.com/LineageOS/android_hardware_qcom_display/blob/lineage-15.1-caf-8916/libcopybit/Android.mk#L33-L49
    
    Change-Id: Icae4433b268fc9a426abcb17d954c5dd2aa1588d

--- msm8916.mk
+++ msm8916.mk
@@ -116,7 +116,6 @@ PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl \
     android.hardware.renderscript@1.0-service \
     gralloc.msm8916 \
-    copybit.msm8916 \
     hwcomposer.msm8916 \
     memtrack.msm8916 \
     libboringssl-compat
