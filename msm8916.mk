1d43835dea349fc10068b1a337eb9b4163edde12
Alberto97 <albertop2197@gmail.com>
2018-06-30 22:49:29 +0200

    Kill power HAL
     * It was designed to provide power profiles. Such feature is not supported by AOSP
    
    # Conflicts:
    #	msm8916.mk

--- msm8916.mk
+++ msm8916.mk
@@ -200,12 +200,6 @@ PRODUCT_PACKAGES += \
     libOmxVidcCommon \
     libstagefrighthw
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
-    power.msm8916
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
