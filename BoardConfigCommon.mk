c67c6f07e04c4b889e0d57ffae57db0c3b2bc594
SubhrajyotiSen <subhrajyoti12@gmail.com>
2018-07-06 19:17:41 +0200

    msm8916-common: drop faceunlock hacks
    Change-Id: I4ce589e240353a78eaf43ca6349018a55242e629

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -156,7 +156,6 @@ LD_SHIM_LIBS := \
     /system/vendor/lib/libmmqjpeg_codec.so|libboringssl-compat.so \
     /system/vendor/lib/libmmcamera_wavelet_lib.so|libc_util.so \
     /system/vendor/lib/libizat_core.so|libshims_get_process_name.so \
-    /system/lib/libfacenet.so|libprotobuf-cpp-haxx.so \
     /system/lib/libmdmcutback.so|libqsap_shim.so
 
 # Wifi
