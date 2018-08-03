208e123db1f6fe84b3b16445f996e10d4985b456
Kyle Harrison <khwebmail@gmail.com>
2018-08-03 16:26:56 +0100

    msm8916-common: Update to Linaro 7.3.1 toolchain

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,8 +46,8 @@ TARGET_CPU_VARIANT := cortex-a53
 TARGET_USES_64_BIT_BINDER := true
 
 # Kernel Toolchain
-ifneq ($(wildcard $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-7.2),)
-  KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-7.2/bin
+ifneq ($(wildcard $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-7.3),)
+  KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-7.3/bin
   KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 endif
 
