aa3b4c9e2e0f849b4382f73145536b103ac405dc
Kevin Rocard <krocard@google.com>
2018-08-21 22:42:45 -0700

    msm8916-common: Migrate to audio V4 implementation
    Test: regression test on the board
    Bug: 38184704
    Change-Id: Ife559125850277865451f92ea862e7ec47fc9da7
    Signed-off-by: Subinsmani <subins.mani@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -54,9 +54,9 @@ PRODUCT_AAPT_CONFIG := normal
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.0-impl \
     sound_trigger.primary \
