22fc131cc9eeb0080c4f2dd660b8e1f30c13a8eb
Sultan Qasim Khan <sultanqasim@lineageos.org>
2018-08-23 23:12:23 -0400

    Revert "msm8916-common: don't build audio HAL on lux"
    Speakerphone mic has been fixed on lux OSS audio HAL.
    
    This reverts commit 1178ee208b6edbee5021315e5b58ec6826b635d5.
    
    Change-Id: I549e5c28e2b2fdd9e70c2dbd7ad46e7db26ea9ad

--- msm8916.mk
+++ msm8916.mk
@@ -61,6 +61,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@2.0-service \
     audio.a2dp.default \
+    audio.primary.msm8916 \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
@@ -68,12 +69,6 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libqcompostprocbundle \
     tinymix
-
-# Only build audio HAL on non-lux devices
-ifeq ($(filter lux,$(TARGET_DEVICE)),)
-PRODUCT_PACKAGES += \
-    audio.primary.msm8916
-endif
 
 PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
