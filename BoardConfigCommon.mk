624fbeb8e5cc9678c270b994f02a0b4e412cf796
Kyle Harrison <khwebmail@gmail.com>
2018-09-17 17:28:28 +0100

    msm8916-common: sepolicy: Enable sepolicy dirs
    - Include legacy qcom sepolicy
    - Re-enable device sepolicy dirs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -152,9 +152,8 @@ TARGET_USES_MKE2FS := true
 TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
-#include device/qcom/sepolicy/legacy-sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+include device/qcom/sepolicy-legacy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
