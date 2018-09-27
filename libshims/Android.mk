ccc9e5287c5501184e411cf07a2f0468593fe17e
razhor <zamorano0695@gmail.com>
2018-09-27 03:08:48 +0000

    Revert "msm-8916-common: Update camera shim for Pie"
    This reverts commit 8d92f8f70f578b5c55120837f7b36cd3f456a6d3.

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -16,7 +16,9 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := MediaCodec.cpp AudioSource.cpp MetaData.cpp
+
+LOCAL_SRC_FILES := MediaCodec.cpp
+
 LOCAL_SHARED_LIBRARIES := libstagefright libmedia
 LOCAL_MODULE := libshims_camera
 LOCAL_MODULE_TAGS := optional
