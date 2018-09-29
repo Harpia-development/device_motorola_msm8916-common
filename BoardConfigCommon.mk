4cc901703f1bba473e8d93d9b05c32ceca250b79
FacuM <facumo.fm@gmail.com>
2018-09-29 09:48:32 +0000

    BoardConfigCommon: don't set toolchain again.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -61,7 +61,6 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8916
-KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 TARGET_EXFAT_DRIVER := sdfat
 BOARD_KERNEL_CMDLINE += pm.sleep_mode=1
