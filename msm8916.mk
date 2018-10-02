4a2b51a8f4c89cb9a2b0a206bb28a62dc3ce40f6
Han Wang <416810799@qq.com>
2018-10-02 10:47:00 +0530

    msm8916-common: Update textclassifier package name for P
    Change-Id: If846cc590060025202fa336af4420747f6b01238

--- msm8916.mk
+++ msm8916.mk
@@ -242,6 +242,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
+    textclassifier.bundle1
 
 # Power
 PRODUCT_PACKAGES += \
