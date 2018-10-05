5f324780e94005ded03ab7fbc0b31d0de5366e83
dev-harsh1998 <harshitjain6751@gmail.com>
2018-10-05 09:55:11 +0530

    harpia: Switch to kernel thermal management
    * MSM simple thermal driver

--- msm8916.mk
+++ msm8916.mk
@@ -229,10 +229,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
-# Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
