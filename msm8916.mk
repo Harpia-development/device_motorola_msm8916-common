fb38d9377056f3e462246de80a400e4530d1ac28
FacuM <facumo.fm@gmail.com>
2018-10-15 23:47:21 +0000

    msm8916: grab media prop from cyanogen msm8916-common.
    Change-Id: I9440928974ce789971724f7958925df430ad3321

--- msm8916.mk
+++ msm8916.mk
@@ -193,6 +193,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.aac_51_output_enabled=true
+
 # Memtrack HAL
 PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
