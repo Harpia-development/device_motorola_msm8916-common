e3fe1d012a8724c84a15796ca22de8b75ce56ca1
Bruno Martins <bgcngm@gmail.com>
2018-11-12 21:18:12 +0000

    msm8916-common: Build ClearKey HIDL plugin from source
     * After all it can be built from source.
    
    Change-Id: Ie0752b3312007c94bc7dd8730fa88bd324a2ac8c

--- msm8916.mk
+++ msm8916.mk
@@ -137,7 +137,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey
 
 # Ebtables
 PRODUCT_PACKAGES += \
