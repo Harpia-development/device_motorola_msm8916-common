54b7b7a2bb62214c2dc78d77162f6dd2d58b5517
eriko <ericfari15@gmail.com>
2019-01-05 23:21:04 -0300

    msm8916-common: disable ZSL to prevent camera vSync issues.

--- msm8916.mk
+++ msm8916.mk
@@ -110,7 +110,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1 \
     media.stagefright.legacyencoder=true \
-    media.stagefright.less-secure=true
+    media.stagefright.less-secure=true \
+    camera.disable_zsl_mode=true
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
