2bff9a44e22535963a2c242690cba8359592494a
Nico <nicorg2529@gmail.com>
2019-01-22 14:42:09 +0000

    msm8916-common: Update RIL to O
    # Conflicts:
    #	manifest.xml
    #	vendor-prop.mk
    
    # Conflicts:
    #	system.prop
    
    # Conflicts:
    #	manifest.xml

--- msm8916.mk
+++ msm8916.mk
@@ -246,7 +246,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
 
 # Thermal
 PRODUCT_COPY_FILES += \
