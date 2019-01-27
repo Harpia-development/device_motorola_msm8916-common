fd1e95edc398a416163f430053d4b57aa821850b
Akianonymus <anonymus.aki@gmail.com>
2019-01-28 00:55:29 +0530

    Move back to userspace thermal implementation
    Revert "Cleanup thermal"
    
    This reverts commit 2c2b3a256dda744312f3eb1942cf29198dbaa2a0.

--- msm8916.mk
+++ msm8916.mk
@@ -281,6 +281,14 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+
 # TextClassifier smart selection model
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
