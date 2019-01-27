0f06b33b6d1fdf41d85deecbd2a209b9b94428b0
Sultan Qasim Khan <sultanqasim@lineageos.org>
2019-01-27 15:50:26 -0500

    msm8916: disable audio effect offload
    They're broken on oreo. Fixes AudioFX.
    Compress offload will still be used when AudioFX is off.
    
    Change-Id: Ia837a87efec590a011fba9f2c210d49fb8cb8f24

--- msm8916.mk
+++ msm8916.mk
@@ -67,7 +67,6 @@ PRODUCT_PACKAGES += \
     libaudio-resampler \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libqcompostprocbundle \
     tinymix
 
 # Only build audio HAL on non-lux devices
