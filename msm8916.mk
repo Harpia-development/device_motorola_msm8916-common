2cde76dedb4ff652a18646d8c666b944ce673125
Akianonymus <anonymus.aki@gmail.com>
2019-03-23 17:12:31 +0530

    msm8916-common: Add aanc_tuning_mixer.txt from caf [1/2]
    * Log:
    MCS-RT-CTL: Can't open the configuration file /system/etc/aanc_tuning_mixer.txt.
    
    * Hex edit libacdbloader.so to load config from vendor
    
    * https://source.codeaurora.org/quic/la/platform/vendor/qcom/msm8916_64/tree/aanc_tuning_mixer.txt?h=LA.BF64.1.1.c3
    
    * Update ims.apk sha1sum while we are at it.

--- msm8916.mk
+++ msm8916.mk
@@ -77,6 +77,7 @@ PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
+    $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/aanc_tuning_mixer.txt \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
