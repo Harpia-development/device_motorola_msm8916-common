faba8b3c61fbd197726cb9e103a72680f42d16aa
Akianonymus <anonymus.aki@gmail.com>
2019-03-23 10:55:29 +0530

    Remove redundant camera packages

--- msm8916.mk
+++ msm8916.mk
@@ -101,12 +101,6 @@ PRODUCT_PACKAGES += \
     libshims_camera \
     libshims_sensorlistener \
     Snap \
-    GoogleCameraMod \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
     libjustshoot_shim
 
 # Camera properties
