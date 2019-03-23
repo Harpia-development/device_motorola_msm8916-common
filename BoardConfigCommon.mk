9f09b381e40b5bf088b69db41b42d5cd78c1f19d
Akianonymus <anonymus.aki@gmail.com>
2019-03-23 10:05:23 +0530

    lib > vendor/lib [3/3]

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -170,13 +170,13 @@ endif
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib/libsensorndkbridge.so|libshim_ril.so \
     /system/vendor/lib/libmdmcutback.so|libqsap_shim.so \
-    /system/lib/libjustshoot.so|libshims_camera.so \
-    /system/lib/libmot_sensorlistener.so|libshims_sensorlistener.so \
+    /system/vendor/lib/libjustshoot.so|libshims_camera.so \
+    /system/vendor/lib/libmot_sensorlistener.so|libshims_sensorlistener.so \
     /system/vendor/lib/libmmcamera_wavelet_lib.so|libshim_atomic.so \
     /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
     /system/vendor/lib/libmmqjpeg_codec.so|libboringssl-compat.so \
     /system/vendor/lib/libmmcamera_wavelet_lib.so|libc_util.so \
-    /system/lib/libjustshoot.so|libjustshoot_shim.so
+    /system/vendor/lib/libjustshoot.so|libjustshoot_shim.so
 
 # Sp-shim
 TARGET_LD_PRELOAD := \
