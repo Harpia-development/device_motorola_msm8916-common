aacf5ba0ed514dd656e0848797ba0b7951db4569
Akianonymus <anonymus.aki@gmail.com>
2019-03-23 15:50:16 +0530

    msm8916-common: Add audio_output_policy.conf from caf
    * Log:
    	E audio_hw_utils: audio_extn_utils_update_streams_output_cfg_list: could not load output policy config file
    
    * https://github.com/Harpia-development/hardware_qcom_audio-caf_msm8916/blob/9/configs/msm8916_32/audio_output_policy.conf

--- msm8916.mk
+++ msm8916.mk
@@ -76,6 +76,7 @@ endif
 PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
