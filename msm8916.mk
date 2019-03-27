42c5c62e15c42400efaeb817f0e708ba002e8076
Flex1911 <dedsa2002@gmail.com>
2019-03-27 08:18:15 +0100

    msm8916-common: Add QCOM's WFD implementation
     * Sadly, out-of-the-box Miracast support was killed in P, so
       we need this to keep Wi-Fi display feature working.
    
    Change-Id: Id680d918b64c0e3733251db45992ec477b6fbdfb
    
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Change-Id: I1c712dba0e4d974a594106034984e1162927ed9c

--- msm8916.mk
+++ msm8916.mk
@@ -254,6 +254,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1
     persist.sys.wfd.virtual=0
 
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libaacwrapper \
+    libnl
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:system/etc/permissions/privapp-permissions-wfd.xml
+
 # Memtrack HAL
 PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
