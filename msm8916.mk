75c24702be7d8471e099f73b7dfe403408a3ac11
Akianonymus <anonymus.aki@gmail.com>
2019-05-06 09:27:13 +0530

    msm8916-common: Remove android.hardware.audio.effect@2.0-service from packages
    * cuz doesn't exist

--- msm8916.mk
+++ msm8916.mk
@@ -61,7 +61,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.common@4.0 \
     android.hardware.audio.common@4.0-util \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.audio.effect@4.0 \
     android.hardware.audio.effect@4.0-impl \
     audio.a2dp.default \
