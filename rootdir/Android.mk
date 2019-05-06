00c9516e713d5b0afb8e140f159e53ca534cde84
Akianonymus <anonymus.aki@gmail.com>
2019-05-06 10:35:33 +0530

    rootdir: Add init.radio.sh  script for qcril.db and fix related denials
    doing directly in init.qcom.rc is a bad idea

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -34,6 +34,14 @@ LOCAL_SRC_FILES    := etc/init.qcom.devstart.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.radio.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.radio.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+include $(BUILD_PREBUILT)
+
 # Init scripts
 
 include $(CLEAR_VARS)
