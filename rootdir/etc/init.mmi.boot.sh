c026c582406c4c1cda0d8c784a53c7ce1cb7aa12
Akianonymus <anonymus.aki@gmail.com>
2019-05-06 10:35:50 +0530

    Cleanup rootdir/etc/init.mmi.boot.sh
    * Unneeded check

--- rootdir/etc/init.mmi.boot.sh
+++ rootdir/etc/init.mmi.boot.sh
@@ -38,29 +38,6 @@ if [[ "$t" != *release* ]]; then
 fi
 unset p v a b t
 
-#
-# Make modem config folder and copy firmware config to that folder for RIL
-#
-if [ -f /data/misc/radio/ver_info.txt ]; then
-    prev_version_info=`cat /data/misc/radio/ver_info.txt`
-else
-    prev_version_info=""
-fi
-
-cur_version_info=`cat /firmware/verinfo/ver_info.txt`
-if [ ! -f /firmware/verinfo/ver_info.txt -o "$prev_version_info" != "$cur_version_info" ]; then
-    rm -rf /data/misc/radio/modem_config
-    mkdir /data/misc/radio/modem_config
-    chmod 770 /data/misc/radio/modem_config
-    cp -r /firmware/image/modem_pr/mcfg/configs/* /data/misc/radio/modem_config
-    chown -hR radio.radio /data/misc/radio/modem_config
-    cp /firmware/verinfo/ver_info.txt /data/misc/radio/ver_info.txt
-    chown radio.radio /data/misc/radio/ver_info.txt
-fi
-cp /firmware/image/modem_pr/mbn_ota.txt /data/misc/radio/modem_config
-chown radio.radio /data/misc/radio/modem_config/mbn_ota.txt
-echo 1 > /data/misc/radio/copy_complete
-
 # Let kernel know our image version/variant/crm_version
 image_version="10:"
 image_version+=`getprop ro.build.id`
