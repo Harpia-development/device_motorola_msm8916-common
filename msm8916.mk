8cb38610b77c8808b8a3d690d4f1ec3a79873f46
Akianonymus <anonymus.aki@gmail.com>
2019-05-10 20:56:00 +0530

    msm8916-common: Bye-bye qti powerhal

--- msm8916.mk
+++ msm8916.mk
@@ -320,7 +320,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # Trust
 PRODUCT_PACKAGES += \
