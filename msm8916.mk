0ad1a9962e9997d7c27c278a27b6c6fca989a077
Your Name <you@example.com>
2019-07-04 17:15:30 -0700

    Binderize ISensor HAL.

--- msm8916.mk
+++ msm8916.mk
@@ -306,6 +306,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
 # Thermal
