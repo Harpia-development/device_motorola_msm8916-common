04a69b797f44aba61aba7a1eda6ee268789e4edc
Akianonymus <anonymus.aki@gmail.com>
2019-09-15 00:00:26 +0530

    Remove thermal hals
    https://del.dog/acowofaxul.sql

--- msm8916.mk
+++ msm8916.mk
@@ -312,6 +312,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     libsensorndkbridge
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # TextClassifier smart selection model
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
