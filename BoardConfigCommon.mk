ddcb93603dbab4a2adf2c74f465e1711359f1ecc
Kyle Harrison <khwebmail@gmail.com>
2019-09-15 21:24:24 +0100

    [TEMP] msm8916-common: Remove boringssl-compat

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -170,9 +170,7 @@ TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib/libmdmcutback.so|libqsap_shim.so \
     /system/lib/libmot_sensorlistener.so|libsensor.so \
     /system/lib/libjustshoot.so|libshims_camera.so \
-    /system/vendor/lib/libmmcamera_wavelet_lib.so|libc_util.so \
-    /system/vendor/lib/libmmqjpeg_codec.so|libboringssl-compat.so \
-    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so
+    /system/vendor/lib/libmmcamera_wavelet_lib.so|libc_util.so
 
 # Telephony
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
--- msm8916.mk
+++ msm8916.mk
@@ -92,7 +92,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.msm8916 \
-    libboringssl-compat \
     libc_util \
     libshims_camera \
     Snap
