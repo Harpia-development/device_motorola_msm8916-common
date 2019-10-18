6036d6111f1e5c694dd50afa41af4e751bbbd9d6
Bruno Martins <bgcngm@gmail.com>
2019-10-18 17:45:11 +0100

    msm8916-common: Drop mediaextractor seccomp policy
    These syscalls are now allowed by default.
    
    [arco] Also clean mediacodec.policy
    
    Change-Id: I405f609d030331e19031612cbd912a54a3c72339

--- msm8916.mk
+++ msm8916.mk
@@ -322,8 +322,7 @@ PRODUCT_PACKAGES += \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Fake QTI jar
 PRODUCT_PACKAGES += \
