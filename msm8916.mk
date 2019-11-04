2109e9b0d8704114fee42f5591d70127df8de647
Kyle Harrison <khwebmail@gmail.com>
2019-11-04 16:44:26 +0000

    Revert "msm8916-common: Switch to local PowerHAL"
    This reverts commit 9e149d3540e2869914a3d5829303af4bbdeb7023.

--- msm8916.mk
+++ msm8916.mk
@@ -355,8 +355,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    power.msm8916
+    android.hardware.power@1.1-service-qti
 
 # USB
 PRODUCT_PACKAGES += \
