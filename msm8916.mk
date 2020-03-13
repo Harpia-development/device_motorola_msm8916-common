e8ccff0ef2247ec5a6f72fc9e08d9103da8b4c10
jro1979oliver <jroliveira.oliveira301@gmail.com>
2020-03-13 16:42:16 +0000

    msm8916-common: add privapp-permission whitelist

--- msm8916.mk
+++ msm8916.mk
@@ -231,6 +231,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Qualcomm
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
